eb47604f641c5029fbf5a0eb60b1f6a4053309b3
박성준 <macleod.park@gmail.com>
2025-05-20 22:35:55 +0900

    tcp

--- tcpserver/src/main.rs
+++ tcpserver/src/main.rs
@@ -1,3 +1,16 @@
+use std::io::{Read, Write};
+use std::net::TcpListener;
+
 fn main() {
-    println!("Hello, world!");
+    // 서버 초기화 후 IP와 포트 3000에 바인딩
+    let connection_listener = TcpListener::bind("127.0.0.1:3000").unwrap();
+    println!("Running on port 3000");
+    // 유입되는 커넥션을 기다림 (listen)
+    for stream in connection_listener.incoming() {
+        let mut stream = stream.unwrap(); // 커넥션 유입
+        println!("Connection established!");
+        let mut buffer = [0; 1024];
+        stream.read(&mut buffer).unwrap();
+        stream.write(&mut buffer).unwrap();
+    }
 }
